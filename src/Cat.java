4ee95d2d23e69a9c77a58debd526d3ad68bf4f94
Tyler Martinez <tymartin@mines.edu>
2016-09-27 12:04:51 -0600

    Moved eyes 6 to the left

--- src/Cat.java
+++ src/Cat.java
@@ -31,7 +31,7 @@ public class Cat {
 		g2.fillOval(x, y, HEAD_DIMENSION, HEAD_DIMENSION);
 		// Draw the eyes
 		g2.setColor(Color.blue);
-		x = catX + EYE_X; 
+		x = catX + EYE_X - 6; 
 		y = catY + EYE_Y;
 		g2.fillOval(x, y, EYE_WIDTH, EYE_HEIGHT);
 		x += EYE_SEPARATION;
